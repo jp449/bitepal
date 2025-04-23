8612aec8d9b1b195a297e9ba7d1a1e0e84b4aefd
aliaziz <ali.hu306@gmail.com>
2025-04-22 23:43:54 -0400

    merge

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from flask_auth_app.mealdb import create_app
 app = create_app()
 if __name__ == '__main__':
     # app.run(debug = True)
-    app.run(debug = True, port=5002)
+    app.run(debug = True, port=5003)
